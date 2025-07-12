13dc02e2f0e276c9794aaf83a520138203c96ed6
Vaz15K <keveneduardobilibio@hotmail.com>
2025-07-12 11:17:08 -0400

    Revert "fs, include, kernel: Implement susfs 1.5.7 patch"
    Just to update SusFS
    
    This reverts commit 3d0b5620732d54e4a8faa67ac2afccf6716b280d.

--- kernel/sys.c
+++ kernel/sys.c
@@ -1305,10 +1305,6 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-extern void susfs_spoof_uname(struct new_utsname* tmp);
-#endif
-
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1317,9 +1313,6 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-	susfs_spoof_uname(&tmp);
-#endif
 	up_read(&uts_sem);
 
 	rcu_read_lock();
